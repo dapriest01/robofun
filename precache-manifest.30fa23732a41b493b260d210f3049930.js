fc8e640d60fab613b86aa442ffa2a418a7fc523e
Oluwafemi <dapriest01@gmail.com>
2019-11-15 11:12:30 +0100

    Testing my automatic robot generator

---
+++ precache-manifest.30fa23732a41b493b260d210f3049930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdb8dcde58b45c3e61e95e1bb05f761",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4df1b472286c409f7f42",
+    "url": "/static/css/main.d1dd65f6.chunk.css"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/static/js/2.d75c0efc.chunk.js"
+  },
+  {
+    "revision": "4df1b472286c409f7f42",
+    "url": "/static/js/main.94ceedb4.chunk.js"
+  },
+  {
+    "revision": "c332e1bb67579b93fb1f",
+    "url": "/static/js/runtime-main.b75238ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
